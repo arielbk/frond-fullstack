a0b1dc2d50b21bd1970bc116eef788760605c185
arielbk <buchwald.kearney@gmail.com>
2021-10-16 11:29:00 +0200

    chore: set up strapi next and tailwind

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9c09829f1d98f2dfaa129b8806e50d13'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3e7fbe0e-bc0e-4317-a6b3-9ca131754f11'
+};
