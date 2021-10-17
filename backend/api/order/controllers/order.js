0199f8b5f9faed9af45613a6b4ae3539d0f14d59
arielbk <buchwald.kearney@gmail.com>
2021-10-17 11:52:44 +0200

    feat: add user orders

--- backend/api/order/controllers/order.js
+++ backend/api/order/controllers/order.js
@@ -1,8 +1,45 @@
-'use strict';
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  /**
+   * only returns orders that belong to the authenticated user
+   * @param {any} ctx
+   */
+  async find(ctx) {
+    const { user } = ctx.state;
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.order.search({
+        ...ctx.query,
+        user: user.id,
+      });
+    } else {
+      entities = await strapi.services.order.find({
+        ...ctx.query,
+        user: user.id,
+      });
+    }
+
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models.order })
+    );
+  },
+
+  /**
+   * returns specific order if it belongs to the authenticated user - else 404
+   * @param {any} ctx
+   */
+  async findOne(ctx) {
+    const { id } = ctx.params;
+    const { user } = ctx.state;
+
+    const entity = await strapi.services.order.findOne({ id, user: user.id });
+    return sanitizeEntity(entity, { model: strapi.models.order });
+  },
+};
--- frontend/pages/account.tsx
+++ frontend/pages/account.tsx
@@ -1,25 +1,95 @@
 import { signOut, useSession } from 'next-auth/client'
 import { Router, useRouter } from 'next/dist/client/router'
+import { useEffect, useState } from 'react'
+import { API_URL, fromImageToUrl } from '../utils/urls'
+
+const useOrders = (session: any) => {
+  const [orders, setOrders] = useState([])
+
+  useEffect(() => {
+    if (!session) return
+    const fetchOrders = async () => {
+      const orderRes = await fetch(`${API_URL}/orders`, {
+        headers: {
+          Authorization: `Bearer ${session.jwt}`,
+        },
+      })
+      const data = await orderRes.json()
+      setOrders(data)
+    }
+
+    fetchOrders()
+  }, [session])
+
+  return orders
+}
 
 export default function Account() {
   const [session, loading] = useSession()
   const router = useRouter()
+  const orders = useOrders(session)
   if (!loading && !session) router.push('/')
   return (
     <div className="bg-gray-50 py-12">
-      <div className="container flex bg-white shadow overflow-hidden sm:rounded-lg w-8/12 p-8 gap-12">
-        {session?.user?.image && (
-          <img
-            src={session?.user?.image}
-            alt=""
-            className="h-36 rounded-full"
-          />
-        )}
+      <h2 className="my-6 text-center text-3xl font-extrabold text-gray-900">
+        Your account
+      </h2>
+      <div className="container flex bg-white shadow overflow-hidden sm:rounded-lg w-8/12 p-8 gap-16">
         <div>
-          <div className="text-xl font-light">{session?.user?.name}</div>
+          {' '}
+          {session?.user?.image && (
+            <img
+              src={session?.user?.image}
+              alt=""
+              className="h-36 rounded-full border-white border-2 shadow-lg"
+            />
+          )}
+          <div className="text-xl font-light mt-6 text-center">
+            {session?.user?.name}
+          </div>
+        </div>
+        <div className="flex-1">
+          <div>
+            <h3 className="text-xl">Orders</h3>
+            {orders.map((order: any) => (
+              <div key={order.id} className="mt-6 border flex-1 rounded-lg p-6">
+                <div className="flex justify-between">
+                  <div className="flex items-center">
+                    <img
+                      src={fromImageToUrl(
+                        order.product.image.formats.thumbnail
+                      )}
+                      alt={order.product.name}
+                      className="h-12 w-12 rounded-full mr-3"
+                    />
+                    <div>
+                      <div>{order.product.name}</div>
+                      <div className="text-gray-400 text-sm">
+                        €{order.product.price}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="text-sm text-gray-400">
+                    {new Date(order.created_at).toLocaleDateString('de')}
+                  </div>
+                </div>
+                <div className="flex justify-between mt-6">
+                  <div className="text-gray-200 bg-gray-600 px-4 rounded-full text-xs flex items-center">
+                    {order.status}
+                  </div>
+                  <div>
+                    <span className="text-xs text-gray-400 mr-2 uppercase">
+                      Total:
+                    </span>{' '}
+                    €{order.total}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
           <button
             onClick={() => signOut()}
-            className="btn mt-8 transition-colors rounded-md px-8 py-2 text-sm bg-white text-green-500 border border-gray-200"
+            className="mt-8 transition-colors rounded-md px-8 py-2 text-sm bg-white text-green-500 border border-gray-200 width-full"
           >
             Logout
           </button>
